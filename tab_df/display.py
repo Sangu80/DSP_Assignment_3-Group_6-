37255e14a5afd5aa2226466a5a867cbd26337bd0
WHITEZOUJUNJIE <133068193+WHITEZOUJUNJIE@users.noreply.github.com>
2023-11-11 11:50:41 +1000

    Create display.py

---
+++ tab_df/display.py
@@ -0,0 +1,39 @@
+import streamlit as st
+
+from tab_df.logics import Dataset
+
+def display_tab_df_content(file_path):
+    # Instantiate the Dataset class and set the data
+    dataset = Dataset(file_path)
+    st.session_state['dataset'] = dataset
+    dataset.set_df()
+    dataset.set_data()
+    dataset.set_dimensions()
+    dataset.set_numeric()
+    dataset.set_columns()
+
+    st.title("Data Analysis - DataFrame")
+
+    # Expander for displaying dataset summary
+    with st.expander("Dataset Summary"):
+        summary_data = dataset.get_summary()
+        st.table(summary_data)
+
+# Expander for selecting rows to display
+    with st.expander("Explore Dataframe"):
+        num_rows = st.slider("Select the number of rows to be displayed", 5, 50, 5)
+        display_method = st.radio("Exploration Method", ("Head", "Tail", "Sample"))
+
+        st.header("Top Rows of Selected Table")
+
+        if display_method == "Head":
+            st.dataframe(dataset.get_head(num_rows))
+        elif display_method == "Tail":
+            st.dataframe(dataset.get_tail(num_rows))
+        else:
+            st.dataframe(dataset.get_sample(num_rows))
+
+    # Display column information
+    st.header("Columns")
+    st.table(dataset.table)
+
